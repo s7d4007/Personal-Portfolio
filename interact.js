c714469c4854f2f41b8405ac3bf7ed19e69ed123
s7d4007 <souvikdutta0775@outlook.com>
2025-02-24 22:13:19 +0530

    feat: enhance UI with CSS improvements and add social media integration
    - Update resume styling and layout
    - Add social media icons (GitHub, LinkedIn, Whatsapp)
    - Integrate motivational quotes

--- interact.js
+++ interact.js
@@ -1,6 +1,6 @@
 document.getElementById("year").textContent = new Date().getFullYear();
 var typed = new Typed('#element', {
-    strings: ['Web Developer', 'Graphic Designer', 'Python Developer'],
+    strings: ['Frontend Developer', 'Graphic Designer', 'Python Developer'],
     typeSpeed: 50,
     backSpeed: 30,       // Speed of deleting text
     loop: true,          // Makes the typing loop indefinitely
